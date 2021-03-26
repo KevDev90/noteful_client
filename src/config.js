5e28e99ccb1088d1f3c6254047f41e5bf67089b1
Kevin Johnson <kevinj902302@gmail.com>
2021-03-26 16:18:06 -0600

    removes console-log

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     API_ENDPOINT: 'https://murmuring-castle-41781.herokuapp.com/api',
-    // API_ENDPOINT: 'https://noteful-client-kevin.herokuapp.com/',
+    // API_ENDPOINT: 'http://localhost:8000/api',
     API_KEY: process.env.REACT_APP_API_KEY,
   }
